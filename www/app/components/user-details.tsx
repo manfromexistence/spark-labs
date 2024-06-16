4ccc26988a7ab0ab7edacdb3199b52ea9fa31a6b
manfromexistence01 Emon <manfromexistence01@gmail.com>
2024-06-16 06:39:06 +0000

    feat: automated commit

--- www/app/components/user-details.tsx
+++ www/app/components/user-details.tsx
@@ -167,25 +167,6 @@ function PointerC({ label }: { label: string }) {
   );
 }
 
-// function formatDate(date: Date) {
-//   return date.toLocaleDateString("en-US", {
-//     month: "short",
-//     day: "numeric",
-//     year: "numeric",
-//   });
-// }
-
-// function formatDateWithNumbers(date: Date): string {
-//   return date.toLocaleString("en-US", {
-//     month: "numeric",
-//     day: "numeric",
-//     year: "numeric",
-//     hour: "numeric",
-//     minute: "2-digit",
-//     second: "2-digit",
-//     hour12: true,
-//   });
-// }
 
 export function UserDetails() {
   const { toast } = useToast()
--- www/app/page.tsx
+++ www/app/page.tsx
@@ -32,6 +32,36 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Separator } from "@/components/ui/separator";
+import { useToast } from "@/registry/default/ui/use-toast"
+import { initializeApp } from "firebase/app"
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  getFirestore,
+  limit,
+  onSnapshot,
+  query,
+  startAfter,
+  updateDoc,
+} from "firebase/firestore"
+const firebaseConfig = {
+  apiKey: "AIzaSyBbh73d_g_CVG0PZPlljzC6d8U-r0DRTFk",
+  authDomain: "snap-workspace.firebaseapp.com",
+  projectId: "snap-workspace",
+  storageBucket: "snap-workspace.appspot.com",
+  messagingSenderId: "1092527848130",
+  appId: "1:1092527848130:web:a6ad15060f8d379b43595b",
+  measurementId: "G-JVEZGJHL8H"
+}
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+// Database
+const db: any = getFirestore(app)
+const auth = getAuth(app);
 
 interface DockProps extends VariantProps<typeof dockVariants> {
   className?: string;
@@ -152,60 +182,6 @@ function DockDemo() {
       <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
         Dock
       </span>
-      {/* 
-      <div className="flex-center border rounded-md h-[50px] w-[50px] hover:bg-primary hover:text-primary-foreground">
-          <MonitorUp />
-        </div>
-        <div className="flex-center border rounded-md h-[50px] w-[50px] hover:bg-primary hover:text-primary-foreground">
-          <Save />
-        </div>
-        <div className="flex-center border rounded-md h-[50px] w-[50px] hover:bg-primary hover:text-primary-foreground">
-          <Send />
-        </div>
-        <div className="flex-center border rounded-md h-[50px] w-[50px] hover:bg-primary hover:text-primary-foreground">
-          <Map />
-        </div>
-        <div className="flex-center border rounded-md h-[50px] w-[50px] hover:bg-primary hover:text-primary-foreground">
-          <CircleDashed />
-        </div>
-        <div className="flex-center border rounded-md h-[50px] w-[50px] hover:bg-primary hover:text-primary-foreground">
-          <ArrowLeft />
-        </div>
-
-        <div className="flex-center border rounded-md h-[50px] w-[50px] hover:bg-primary hover:text-primary-foreground">
-          <ChevronsRightLeft />
-        </div> */}
-
-      {/* <Dock>
-                <DockIcon onClick={() => setSubmitBar(!submitBar)} className={cn(submitBar ? "jello-vertical" : "")}>
-          {submitBar ? <X className="h-4 w-4" /> : 
-          <MonitorUp />
-        </DockIcon>
-                <DockIcon onClick={() => setSubmitBar(!submitBar)} className={cn(submitBar ? "jello-vertical" : "")}>
-          {submitBar ? <X className="h-4 w-4" /> : 
-          <Save />
-        </DockIcon>
-                <DockIcon onClick={() => setSubmitBar(!submitBar)} className={cn(submitBar ? "jello-vertical" : "")}>
-          {submitBar ? <X className="h-4 w-4" /> : 
-          <Save />
-        </DockIcon>
-                <DockIcon onClick={() => setSubmitBar(!submitBar)} className={cn(submitBar ? "jello-vertical" : "")}>
-          {submitBar ? <X className="h-4 w-4" /> : 
-          <Map />
-        </DockIcon>
-                <DockIcon onClick={() => setSubmitBar(!submitBar)} className={cn(submitBar ? "jello-vertical" : "")}>
-          {submitBar ? <X className="h-4 w-4" /> : 
-          <CircleDashed />
-        </DockIcon>
-                <DockIcon onClick={() => setSubmitBar(!submitBar)} className={cn(submitBar ? "jello-vertical" : "")}>
-          {submitBar ? <X className="h-4 w-4" /> : 
-          <ArrowLeft />
-        </DockIcon>
-                <DockIcon onClick={() => setSubmitBar(!submitBar)} className={cn(submitBar ? "jello-vertical" : "")}>
-          {submitBar ? <X className="h-4 w-4" /> : 
-          <ChevronsRightLeft />
-        </DockIcon>
-      </Dock> */}
     </div>
   );
 }
@@ -221,6 +197,65 @@ export default function Home() {
   const [detailsBar, setDetailsBar] = useState(false);
   const [developerBar, setDeveloperBar] = useState(false);
   const [actionsBar, setActionsBar] = useState(false);
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [classroom, setClassroom] = useState("");
+  const [xml, setXml] = useState("");
+  const [time, setTime] = useState(new Date());
+
+  const [projectStatus, setProjectStatus] = useState<any>(false);
+  const [projectId, setProjectId] = useState<any>("");
+
+  const { toast } = useToast()
+  const router = useRouter()
+  const buttonRef = useRef<HTMLButtonElement | null>(null)
+
+  const submitProject = async () => {
+    const { clientWidth, clientHeight } = document.documentElement
+    const boundingBox = buttonRef.current?.getBoundingClientRect?.()
+    const targetY = boundingBox?.y ?? 0
+    const targetX = boundingBox?.x ?? 0
+    const targetWidth = boundingBox?.width ?? 0
+    const targetCenterX = targetX + targetWidth / 2
+    const confetti = (await import("canvas-confetti")).default;
+    setTime(new Date());
+    confetti({
+      zIndex: 9999,
+      particleCount: 100,
+      spread: 100,
+      origin: {
+        y: targetY / clientHeight,
+        x: targetCenterX / clientWidth,
+      },
+    })
+    const Create = await addDoc(collection(db, "submissions"), {
+      xml: xml,
+      title: title,
+      description: description,
+      classroom: classroom,
+      user: auth.currentUser && auth.currentUser.uid,
+      time: time,
+    })
+    console.log("Document written with ID: ", Create.id)
+    toast({
+      title: "Submissions has been added!",
+      description: (
+        <div className="mt-2 w-[340px] rounded-md bg-primary-foreground p-4">
+          <span>You Can now update,view and delete this specialties!</span>
+          <pre className="max-h-[500px] overflow-auto bg-background">
+            <code className="bg-secondary text-muted-foreground">
+              {JSON.stringify(Create.id, null, 2)}
+            </code>
+          </pre>
+        </div>
+      ),
+    });
+    // router.push("/specialties");
+    projectStatus(true);
+    projectId(Create.id);
+  }
+
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -341,34 +376,6 @@ export default function Home() {
 
         </main>
       </div> */}
-
-      {/* <div className="flex flex-row items-center justify-center space-x-3 h-16 w-fit rounded-md border fixed bottom-5 left-1/2 transform -translate-x-1/2 px-1.5">
-        <div className="flex-center border rounded-md h-[50px] w-[50px] hover:bg-primary hover:text-primary-foreground">
-          <MonitorUp />
-        </div>
-        <div className="flex-center border rounded-md h-[50px] w-[50px] hover:bg-primary hover:text-primary-foreground">
-          <Save />
-        </div>
-        <div className="flex-center border rounded-md h-[50px] w-[50px] hover:bg-primary hover:text-primary-foreground">
-          <Send />
-        </div>
-        <div className="flex-center border rounded-md h-[50px] w-[50px] hover:bg-primary hover:text-primary-foreground">
-          <Map />
-        </div>
-        <div className="flex-center border rounded-md h-[50px] w-[50px] hover:bg-primary hover:text-primary-foreground">
-          <CircleDashed />
-        </div>
-        <div className="flex-center border rounded-md h-[50px] w-[50px] hover:bg-primary hover:text-primary-foreground">
-          <ArrowLeft />
-        </div>
-
-        <div className="flex-center border rounded-md h-[50px] w-[50px] hover:bg-primary hover:text-primary-foreground">
-          <ChevronsRightLeft />
-        </div>
-
-      </div> */}
-
-
       <div className={cn("min-h-[500px] w-[345px] rounded-md fixed bottom-12 left-[calc(50%-172.5px)] transform items-end bg-background", submitBar ? "scale-in-ver-bottom" : "slide-out-blurred-top")}>
         <Card className="w-full pb-5">
           <CardHeader>
@@ -398,7 +405,7 @@ export default function Home() {
                 </SelectContent>
               </Select>
             </div>
-            <Button type="submit" className="w-full hover:bg-primary-foreground hover:text-primary">
+            <Button ref={buttonRef} onClick={submitProject} type="submit" className="relative w-full hover:bg-primary-foreground hover:text-primary">
               <Send className="h-4 w-4 mr-2" />
               Submit Project
             </Button>
@@ -571,13 +578,31 @@ export default function Home() {
         <DockIcon onClick={() => setSubmitBar(!submitBar)} className={cn(submitBar ? "jello-vertical" : "")}>
           {submitBar ? <X className="h-4 w-4" /> : <Send className="h-4 w-4" />}
         </DockIcon>
-        <DockIcon onClick={() => setSubmitBar(!submitBar)} className={cn(submitBar ? "jello-vertical" : "")}>
-          {submitBar ? <X className="h-4 w-4" /> :
-            <MonitorUp className="h-4 w-4" />}
+        <DockIcon onClick={() => {
+          console.log("Load");
+          toast({
+            title: "Load Your Project!",
+            description: (
+              <div className="mt-2 w-[340px] rounded-md bg-primary-foreground p-4">
+                <span>You have to follow the Load Menu to load your project.</span>
+              </div>
+            ),
+          });
+        }} className={cn("hover:jello-vertical")}>
+          <MonitorUp className="h-4 w-4" />
         </DockIcon>
-        <DockIcon onClick={() => setSubmitBar(!submitBar)} className={cn(submitBar ? "jello-vertical" : "")}>
-          {submitBar ? <X className="h-4 w-4" /> :
-            <Save className="h-4 w-4" />}
+        <DockIcon onClick={() => {
+          console.log("Save");
+          toast({
+            title: "Save Your Project!",
+            description: (
+              <div className="mt-2 w-[340px] rounded-md bg-primary-foreground p-4">
+                <span>You have to follow the Save Menu to save your project.</span>
+              </div>
+            ),
+          });
+        }} className={cn("hover:jello-vertical")}>
+          <Save className="h-4 w-4" />
         </DockIcon>
         <DockIcon onClick={() => setNavigationsBar(!navigationsBar)} className={cn(submitBar ? "jello-vertical" : "")}>
           {navigationsBar ? <X className="h-4 w-4" /> :
@@ -602,6 +627,8 @@ export default function Home() {
 
 
 import type { SVGProps } from "react";
+import { useRouter } from "next/router";
+import { getAuth } from "firebase/auth";
 // const Instagram = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" preserveAspectRatio="xMidYMid" viewBox="0 0 256 256" {...props}><path fill="#0A0A08" d="M128 23.064c34.177 0 38.225.13 51.722.745 12.48.57 19.258 2.655 23.769 4.408 5.974 2.322 10.238 5.096 14.717 9.575 4.48 4.479 7.253 8.743 9.575 14.717 1.753 4.511 3.838 11.289 4.408 23.768.615 13.498.745 17.546.745 51.723 0 34.178-.13 38.226-.745 51.723-.57 12.48-2.655 19.257-4.408 23.768-2.322 5.974-5.096 10.239-9.575 14.718-4.479 4.479-8.743 7.253-14.717 9.574-4.511 1.753-11.289 3.839-23.769 4.408-13.495.616-17.543.746-51.722.746-34.18 0-38.228-.13-51.723-.746-12.48-.57-19.257-2.655-23.768-4.408-5.974-2.321-10.239-5.095-14.718-9.574-4.479-4.48-7.253-8.744-9.574-14.718-1.753-4.51-3.839-11.288-4.408-23.768-.616-13.497-.746-17.545-.746-51.723 0-34.177.13-38.225.746-51.722.57-12.48 2.655-19.258 4.408-23.769 2.321-5.974 5.095-10.238 9.574-14.717 4.48-4.48 8.744-7.253 14.718-9.575 4.51-1.753 11.288-3.838 23.768-4.408 13.497-.615 17.545-.745 51.723-.745M128 0C93.237 0 88.878.147 75.226.77c-13.625.622-22.93 2.786-31.071 5.95-8.418 3.271-15.556 7.648-22.672 14.764C14.367 28.6 9.991 35.738 6.72 44.155 3.555 52.297 1.392 61.602.77 75.226.147 88.878 0 93.237 0 128c0 34.763.147 39.122.77 52.774.622 13.625 2.785 22.93 5.95 31.071 3.27 8.417 7.647 15.556 14.763 22.672 7.116 7.116 14.254 11.492 22.672 14.763 8.142 3.165 17.446 5.328 31.07 5.95 13.653.623 18.012.77 52.775.77s39.122-.147 52.774-.77c13.624-.622 22.929-2.785 31.07-5.95 8.418-3.27 15.556-7.647 22.672-14.763 7.116-7.116 11.493-14.254 14.764-22.672 3.164-8.142 5.328-17.446 5.95-31.07.623-13.653.77-18.012.77-52.775s-.147-39.122-.77-52.774c-.622-13.624-2.786-22.929-5.95-31.07-3.271-8.418-7.648-15.556-14.764-22.672C227.4 14.368 220.262 9.99 211.845 6.72c-8.142-3.164-17.447-5.328-31.071-5.95C167.122.147 162.763 0 128 0Zm0 62.27C91.698 62.27 62.27 91.7 62.27 128c0 36.302 29.428 65.73 65.73 65.73 36.301 0 65.73-29.428 65.73-65.73 0-36.301-29.429-65.73-65.73-65.73Zm0 108.397c-23.564 0-42.667-19.103-42.667-42.667S104.436 85.333 128 85.333s42.667 19.103 42.667 42.667-19.103 42.667-42.667 42.667Zm83.686-110.994c0 8.484-6.876 15.36-15.36 15.36-8.483 0-15.36-6.876-15.36-15.36 0-8.483 6.877-15.36 15.36-15.36 8.484 0 15.36 6.877 15.36 15.36Z" /></svg>;
 const Facebook = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36" fill="url(#a)" height="1em" width="1em" {...props}><defs><linearGradient x1="50%" x2="50%" y1="97.078%" y2="0%" id="a"><stop offset="0%" stopColor="#0062E0" /><stop offset="100%" stopColor="#19AFFF" /></linearGradient></defs><path d="M15 35.8C6.5 34.3 0 26.9 0 18 0 8.1 8.1 0 18 0s18 8.1 18 18c0 8.9-6.5 16.3-15 17.8l-1-.8h-4l-1 .8z" /><path fill="#FFF" d="m25 23 .8-5H21v-3.5c0-1.4.5-2.5 2.7-2.5H26V7.4c-1.3-.2-2.7-.4-4-.4-4.1 0-7 2.5-7 7v4h-4.5v5H15v12.7c1 .2 2 .3 3 .3s2-.1 3-.3V23h4z" /></svg>;
 const YouTube = (props: SVGProps<SVGSVGElement>) => <svg viewBox="0 0 256 180" width="1em" height="1em" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" {...props}><path d="M250.346 28.075A32.18 32.18 0 0 0 227.69 5.418C207.824 0 127.87 0 127.87 0S47.912.164 28.046 5.582A32.18 32.18 0 0 0 5.39 28.24c-6.009 35.298-8.34 89.084.165 122.97a32.18 32.18 0 0 0 22.656 22.657c19.866 5.418 99.822 5.418 99.822 5.418s79.955 0 99.82-5.418a32.18 32.18 0 0 0 22.657-22.657c6.338-35.348 8.291-89.1-.164-123.134Z" fill="red" /><path fill="#FFF" d="m102.421 128.06 66.328-38.418-66.328-38.418z" /></svg>;
